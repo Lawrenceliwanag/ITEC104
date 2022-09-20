ce092cb1cfe37bffbfe9a346bfa30545e09a75c1
Lawrenceliwanag <112452327+Lawrenceliwanag@users.noreply.github.com>
2022-09-20 17:58:41 +0800

    Create ITEC104_ACTIVITY4_LIWANAG.cs

---
+++ ITEC104_ACTIVITY4_LIWANAG.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+
+class Program
+{
+        static void Main() {
+        int[] num = new int[5];
+        int i=0;
+      	int[] even = new int[0];
+      	int[] odd = new int[0];
+        
+        while(i!=num.Length){
+            Console.Write("input number: ");
+            num[i] = Convert.ToInt32(Console.ReadLine());
+            
+	            if(num[i]%2==0){
+            	Array.Resize(ref even, even.Length+1);
+            	even[even.Length-1] = num[i];
+	            }
+	            else{
+            	Array.Resize(ref odd, odd.Length+1);
+	            odd[odd.Length-1] = num[i];	
+	            }
+            Console.Clear();
+            i++;
+        }
+      	Console.Write("Even: ");
+      	foreach(int item in even){
+      		Console.Write(item+" ");
+      	}
+      	Console.Write("Odd: ");
+      	foreach(int item in odd){
+      		Console.Write(item+" ");
+      	}
+      	
+      	Console.ReadLine();
+    }
+}
