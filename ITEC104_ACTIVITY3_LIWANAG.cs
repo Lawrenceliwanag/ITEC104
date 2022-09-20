613f34d78cc2b84dd940eba143a9dacb9eb61321
Lawrenceliwanag <112452327+Lawrenceliwanag@users.noreply.github.com>
2022-09-20 17:28:36 +0800

    Create ITEC104_ACTIVITY3_LIWANAG.cs

---
+++ ITEC104_ACTIVITY3_LIWANAG.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ITEC104_ACTIVITY3_LIWANAG
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			string[] email = new string[]{"reg.realeza@gmail.com"};
+			string[] username = new string[]{"rreginarose"};
+			string[] password = new string[]{"Yeyel"};
+			
+			Console.WriteLine("Email: {0}",email[0]);
+			Console.WriteLine("Username: {0}",username[0]);
+			Console.WriteLine("Password: {0}",password[0]);
+			
+			Console.ReadLine();
+			
+		}
+	}
+}
